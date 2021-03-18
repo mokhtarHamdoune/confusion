f110637cc00028aff4b8c2a1876b44d5310d18c5
mokhtarHamdoune <mokhtardark@gmail.com>
2021-03-18 12:28:25 +0100

    Assignment 3 : add a comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { Card,CardBody,CardText,CardImg,CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import { Card,CardBody,CardText,CardImg,CardTitle, Breadcrumb, BreadcrumbItem,
+    Modal,ModalHeader,ModalBody,Button, Row, Col,Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
+import {LocalForm,Control,Errors} from 'react-redux-form';
+
+//validation functions 
+const required = (val) => val && val.length;
+const minLength = len => val => (val) && val.length >= len;
+const maxLength = len => val => !(val) ||  val.length <= len ;
+
 function RenderDish({dish}){
     return (
         <Card>
@@ -15,26 +23,122 @@ function RenderDish({dish}){
 
 function RenderComments({comments}){
     return(
-        
-        <ul className='list-unstyled '>
+        <div className="row">
+            <div className="col-12">
+                <ul className='list-unstyled '>
+                        {
+                            comments.map(({id,comment,author,date})=>{
+                                return (
+                                    <li key={id}>
+                                        <p>
+                                            {comment}
+                                        </p>
+                                        <p>
+                                            --{author},{new Date(date).toDateString()}
+                                        </p>
+                                    </li>
+                                )
+                            })
+                        }
+                </ul>
                 {
-                    comments.map(({id,comment,author,date})=>{
-                        return (
-                            <li key={id}>
-                                <p>
-                                    {comment}
-                                </p>
-                                <p>
-                                    --{author},{new Date(date).toDateString()}
-                                </p>
-                            </li>
-                        )
-                    })
+                    <CommentForm />
                 }
-        </ul>
+            </div>
+                        
+        </div>
 
     )
 }
+
+class CommentForm  extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            isModalOpen:false
+        }
+        this.toggleModal=this.toggleModal.bind(this);
+        this.handleSumbit= this.handleSumbit.bind(this);
+    }
+    toggleModal(){
+        this.setState({isModalOpen:!this.state.isModalOpen});
+    }
+    handleSumbit(values){
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        this.toggleModal();
+    }
+
+    render(){
+        return(
+            <div className="row">
+                <div className="col-12">
+                    <Button color="secondary" outline onClick={this.toggleModal} >
+                        <span className="fa fa-edit fa-lg"></span>
+                        Submit Comment
+                    </Button>
+                </div>
+                <div className="col-12">
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal} >
+                        <ModalHeader>SumbitComment</ModalHeader>
+                        <ModalBody>
+                            <LocalForm onSubmit={(values)=>this.handleSumbit(values)}>
+                                <Row className="form-group">
+                                    <Label for="rating" sm={12}>Rating</Label>
+                                    <Col sm={12}>
+                                        <Control.select  model='.rating'
+                                         name='rating'
+                                         id="rating" className="custom-select" >
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </Control.select>
+                                    </Col>
+                                </Row>
+                                <Row className="form-group">
+                                    <Label for="name" sm={12}>Your name</Label>
+                                    <Col sm={12}>
+                                        <Control.text  model='.name'
+                                         name='name'
+                                         id="name" className="form-control"
+                                         validators={{
+                                             required,minLength:minLength(3),maxLength:maxLength(15)
+                                         }}
+                                         />
+                                        <Errors 
+                                            className="text-danger"
+                                            model='.name'
+                                            show='touched'
+                                            
+                                            messages={{
+                                                required:'author name is required, ',
+                                                minLength:'must be greater than 2 characters, ',
+                                                maxLength:'must be less than 15 characters'
+                                            }}
+                                        />
+                                    </Col>
+                                </Row>
+                                <Row className="form-group">
+                                    <Label for="comment" sm={12}>Comment</Label>
+                                    <Col sm={12}>
+                                        <Control.textarea rows={6}  model='.comment'
+                                         name='comment'
+                                         id="comment" className="form-control" />
+                                    </Col>
+                                </Row>
+                                <Button type="submit" color="primary">Submit</Button>
+                            </LocalForm>
+                        </ModalBody>
+                    </Modal>
+                </div>
+
+            </div>
+        );
+    }
+}
+
 
 const  DishDetail = (props)=>{
     return (
@@ -65,3 +169,4 @@ const  DishDetail = (props)=>{
 
 
 export default DishDetail;
+
